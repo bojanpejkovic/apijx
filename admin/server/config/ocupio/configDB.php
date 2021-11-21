fc621be5f78d3a769463586603b326913a70d09a
bojanpejkovic <bojan.pejkovic.itbc@gmail.com>
2021-11-21 18:05:40 +0100

    old files delete

--- admin/server/config/ocupio/configDB.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function configDB(){
- 	return array(
-		"sa"=>array( "db_host"=>"localhost","db_user"=>"root","db_pass"=>"","db_name"=>"ocupio" ), 
-		"admin"=>array( "db_host"=>"localhost","db_user"=>"admin","db_pass"=>"admin","db_name"=>"ocupio" ), 
-		"regular"=>array( "db_host"=>"localhost","db_user"=>"regular","db_pass"=>"regular","db_name"=>"ocupio" ) 
-	);  
-}  
-?>
